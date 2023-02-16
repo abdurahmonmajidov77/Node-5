973061bff383b31df4923ba1695f9af58e4b463b
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-16 16:32:35 +0500

    hi

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ form.addEventListener('submit', e => {
     const name = e.target[0].value;
     const country = e.target[1].value;
 })
+
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const ListElem = require('./function.js')
+
+const Home = fs.readFileSync('./index.html', 'utf-8', (err,data) => {
+    if(err) throw err
+    return data
+})
+const a= JSON.parse(res)
+
+http.createServer(function(req,res){
+    const {pathname, query} = url.parse(req.url, true)
+    if(pathname=='/'){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.write(ListElem(a))
+        res.end(Home)
+    }
+}).listen(1001)
